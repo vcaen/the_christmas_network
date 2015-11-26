f998c89b22b8173cdd77e4f6267049912d27894c
Vadim Caen <vadimcaen@gmail.com>
2015-11-26 11:04:58 +0100

    conf ok

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+setup(name='Krutherbes',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
+      description='Krutherbes Website',
+      author='Your Name',
+      author_email='vaimdcaen@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
+#      install_requires=['Django>=1.3'],
      )
